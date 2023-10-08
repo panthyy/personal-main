df200e59eb60f29578c5f704a28a1271da85276d
panthyy <fahad.alhatshami@hotmail.com>
2023-10-08 22:21:21 +0200

    🚀 Launch push

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,30 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			animation: {
+				// fadeIn 0.45s ease-in-out 0.5s both;
+				'fade-in': 'fadeIn 0.45s ease-in-out both',
+				'fade-in-1': 'fadeIn 0.45s ease-in-out 0.5s both',
+				'fade-in-2': 'fadeIn 0.45s ease-in-out 0.7s both',
+				'fade-in-3': 'fadeIn 0.45s ease-in-out 0.9s both',
+				'fade-in-4': 'fadeIn 0.45s ease-in-out 1.5s both',
+				'fade-in-5': 'fadeIn 0.45s ease-in-out 2s both',
+				'slide-in': 'slideIn 0.45s ease-in-out both',
+				'slide-in-1': 'slideIn 0.45s ease-in-out 0.5s both',
+				'slide-in-2': 'slideIn 0.45s ease-in-out 0.7s both',
+				'slide-in-3': 'slideIn 0.45s ease-in-out 0.9s both',
+				'slide-in-4': 'slideIn 0.45s ease-in-out 1.5s both',
+			},
+			keyframes: {
+				fadeIn: {
+					'0%': { opacity: '0' },
+					'100%': { opacity: '1' },
+				},
+				slideIn: {
+					'0%': { transform: 'translateX(-100%)' , opacity: '0' },
+					'100%': { transform: 'translateX(0)', opacity: '1' },
+				}
+			},
 			colors: {
 				black: "#333333",
 				white: "#FFFFFF",
@@ -25,8 +49,8 @@ module.exports = {
 					color: '#333333',
 					zIndex: '10',
 				},
-				'h1,h2,h3,h4,h5,h6': { fontWeight: '700', fontFamily: 'Georgia, serif' },
-				'p': { fontFamily: 'Arial, sans-serif' },
+				'h1,h2,h3,h4,h5,h6': { fontWeight: '700', fontFamily: 'Georgia, serif' , marginTop: '16px'},
+				'p': { fontFamily: 'Arial, sans-serif' , marginTop: '8px'},
 				'h1,h2,h3': { lineHeight: '1.2' },
 				'h4,h5,h6': { lineHeight: '1.4' },
 				'h1': { fontSize: '3.5rem' },
