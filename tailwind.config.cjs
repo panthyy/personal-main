12708bc5dca1e109233415f56952b952a6a3920a
panthyy <fahad.alhatshami@hotmail.com>
2023-10-08 05:43:52 +0200

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,47 @@
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				black: "#333333",
+				white: "#FFFFFF",
+				grey: "#F4F4F4",
+				primary: "#2A4C6F",
+				secondary: "#E5EDF5",
+				accent: "#0973DC",
+			}
+		},
+	},
+	plugins: [
+		plugin(function ({ addBase }) {
+			addBase({
+				'*': {
+					color: '#333333',
+				},
+				'h1,h2,h3,h4,h5,h6': { fontWeight: '700', fontFamily: 'Georgia, serif' },
+				'p': { fontFamily: 'Arial, sans-serif' },
+				'h1,h2,h3': { lineHeight: '1.2' },
+				'h4,h5,h6': { lineHeight: '1.4' },
+				'h1': { fontSize: '3.5rem' },
+				'h2': { fontSize: '3rem' },
+				'h3': { fontSize: '2.5rem' },
+				'h4': { fontSize: '2rem' },
+				'h5': { fontSize: '1.5rem' },
+				'h6': { fontSize: '1.25rem' },
+				'a': { fontFamily: 'Arial, sans-serif' },
+				'@media (max-width: 768px)': {
+					'h1': { fontSize: '2.5rem' },
+					'h2': { fontSize: '2.25rem' },
+					'h3': { fontSize: '2rem' },
+					'h4': { fontSize: '1.5rem' },
+					'h5': { fontSize: '1.25rem' },
+					'h6': { fontSize: '1.25rem' },
+				},
+
+			})
+		})
+	],
+}
